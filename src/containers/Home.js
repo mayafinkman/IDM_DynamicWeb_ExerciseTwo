17389b68dd218c8402f2a5d3447cb68486372af5
mayafinkman <59428672+mayafinkman@users.noreply.github.com>
2020-02-26 09:29:40 -0500

    Add axios and API key

---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    const defaultKey = "bf51228f98f258e444dcebb583558d21";
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+export default Home;
